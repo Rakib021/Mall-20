43f7627dfa8f83fb27b40b3d66a0a5aa315ec756
Rakib <rakib@Rakibs-MacBook-Pro.local>
2021-06-14 11:38:41 +0600

    server code

--- index.js
+++ index.js
@@ -32,14 +32,14 @@ app.get("/", (req, res) => {
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cpqmt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-// console.log(uri);
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 client.connect((err) => {
-  console.log("Connection Error", err);
+  console.log("Connection Error..please try again", err);
 
   const productsCollection = client.db("mall20Store").collection("products");
   
